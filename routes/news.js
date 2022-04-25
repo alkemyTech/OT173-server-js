b2cc11fe37b43aea24b412dd39a40d769479722d
Juanhernandezzs <juanmhdz99@gmail.com>
2022-04-25 19:17:11 -0300

    update: Now returns an id

--- routes/news.js
+++ routes/news.js
@@ -6,6 +6,7 @@ router.get('/', async function (req, res, next) {
     try {
         const allNews = await Entries.findAll({ where: { type: 'news' } })
         const news = allNews.map(n => {
+            n.id,
             n.name,
             n.image,
             n.createdAt
