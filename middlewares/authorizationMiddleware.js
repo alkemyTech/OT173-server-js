ba95ae8fbbed96bbf4e33b3ae32a73416b762ea3
Juanhernandezzs <juanmhdz99@gmail.com>
2022-04-25 23:36:27 -0300

    update: Created authorization middleware

---
+++ middlewares/authorizationMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const httpCodes = require('../constants/constants')
+
+module.exports = () => {
+    return (req, res, next) => {
+        const token = req.headers['authorization']
+        if (!token) {
+            return res.status(httpCodes.UNAUTHORIZED).send('Access denied')
+        } else {
+            const tokenBody = token.slice(7)
+
+            jwt.verify(tokenBody, 'secretKey', (err, decoded) => {
+                if (err) {
+                    console.log(`JWT error: ${err}`)
+                    return res.status(httpCodes.UNAUTHORIZED).send('Access denied')
+                }
+
+                if (decoded.user && decoded.user.roleId === 1) {
+                    next()
+                } else {
+                    return res.status(httpCodes.UNAUTHORIZED).send('Access denied')
+                }
+            })
+        }
+    }
+}
