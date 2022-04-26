b837e88f1e0fd246d39d22386207ed28a7550b92
Nicolás Cabello <cabellonic@gmail.com>
2022-04-26 00:25:44 -0300

    update: Add Internal server error code to the httpCodes object in constants

--- constants/constants.js
+++ constants/constants.js
@@ -1,8 +1,9 @@
 const httpCodes = {
-    OK : 200,
-    BAD_REQUEST: 400,
-    UNAUTHORIZED: 401,
-    NOT_FOUND : 404,
- }
+  OK: 200,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+};
 
- module.exports = httpCodes;
+module.exports = httpCodes;
