5a030443ec2187c75fe4541dae70440bb48ed710
Facundo Villarreal <facundovillarreal@mangoodev.com>
2022-04-27 22:13:28 -0300

    se recupero archivo que no debia elimnarse

--- seeders/20220421153125-create-slide-test.js
+++ seeders/20220421153125-create-slide-test.js
@@ -2,12 +2,11 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('news', [{
-      name: 'Pedro Picapiedra',
-      content: 'Abcdefghij',
-      image: 'https://organizacionsomosmas.com/wp-content/themes/organizacion-somos-mas/assets/3-logo-somos-mas.png',
-      categoryId: 2,
-      type: 'abcdfgihj',
+    await queryInterface.bulkInsert('Slide', [{
+      imageUrl: 'https://organizacionsomosmas.com/wp-content/themes/organizacion-somos-mas/assets/3-logo-somos-mas.png',
+      text: 'Logo',
+      order: '1',
+      organizationId: '1'
     }], {});
   },
 
