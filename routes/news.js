eced079f04644fbf9061a3133754bbc9d5e1c7da
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-04-28 21:30:29 -0300

    updated: PUT New Route

--- routes/news.js
+++ routes/news.js
@@ -1,20 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { Entries } = require('../models/index.js')
+const { Entries } = require("../models/index.js");
+const httpCodes = require("../constants/constants");
+router.get("/", async function (req, res, next) {
+  try {
+    const allNews = await Entries.findAll({ where: { type: "news" } });
+    const news = allNews.map((n) => {
+      n.id, n.name, n.image, n.createdAt;
+    });
+    res.send(news);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
-router.get('/', async function (req, res, next) {
-    try {
-        const allNews = await Entries.findAll({ where: { type: 'news' } })
-        const news = allNews.map(n => {
-            n.id,
-            n.name,
-            n.image,
-            n.createdAt
-        })
-        res.send(news);
-    } catch (error) {
-        res.send(error)
-    }
+router.put("/:id", async (req, res, next) => {
+  const { name, content, image, type } = req.body;
+  const { id } = req.params;
+  try {
+    const updatedNew = await Entries.update(
+      {
+        name,
+        content,
+        image,
+        type,
+      },
+      {
+        where: {
+          categoryId: id,
+        },
+        returning: true,
+        plain: true,
+      }
+    );
+    if (!updatedNew)
+      return res
+        .status(httpCodes.UNAUTHORIZED)
+        .json({ msg: "Invalid username or password" });
+    res.status(httpCodes.OK).json(updatedNew);
+  } catch (err) {
+    res.status(httpCodes.BAD_REQUEST).json({ error, ok: false });
+  }
 });
 
 module.exports = router;
