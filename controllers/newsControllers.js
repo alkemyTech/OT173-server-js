978db4ae047c561af39e9a5d18a2d1bbd290b657
Nicolás Cabello <cabellonic@gmail.com>
2022-04-30 17:13:58 -0300

    update: Add postNews method to newsControllers.js

--- controllers/newsControllers.js
+++ controllers/newsControllers.js
@@ -1,5 +1,5 @@
 const httpCodes = require("../constants/constants");
-const { Entries } = require('../models/index.js');
+const { Entries, Categories } = require('../models/index.js');
 
 const getNewsById = async (req, res, next) => {
     try {
@@ -24,6 +24,42 @@ const getNewsById = async (req, res, next) => {
     }
 };
 
+const postNews = async (req, res) => {
+  const newsToInsert = {
+    name: req.body.name,
+    content: req.body.content,
+    image: req.body.image,
+    type: 'news',
+  };
+
+  try {
+    const categoryFromDB = await Categories.findOne({
+      where: { name: req.body.category },
+    });
+    if (!categoryFromDB)
+      return res
+        .status(httpCodes.BAD_REQUEST)
+        .json({ msg: 'Category not found' });
+
+    const insertedNews = await Entries.create({
+      ...newsToInsert,
+      categoryId: categoryFromDB.dataValues.id,
+    });
+
+    return res.status(httpCodes.OK).json({
+      ok: true,
+      message: 'News created successfully',
+      news: insertedNews,
+    });
+  } catch (error) {
+    return res.status(httpCodes.INTERNAL_SERVER_ERROR).json({
+      ok: false,
+      error,
+    });
+  }
+};
+
 module.exports = {
     getNewsById,
+    postNews,
 }
