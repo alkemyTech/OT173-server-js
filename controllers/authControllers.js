f9703b8296aca437804c51eabfb20c9e5fbb18f5
Nicolás Cabello <cabellonic@gmail.com>
2022-05-01 18:44:37 -0300

    update: Format code with prettier.

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -3,41 +3,48 @@ const bcrypt = require('bcryptjs');
 const httpCodes = require('../constants/constants');
 
 const createUser = async (req, res, next) => {
-    try {
-        const { firstName, lastName, email, password } = req.body;
-        const passwordHash = await encryptPassword(password);
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    const passwordHash = await encryptPassword(password);
 
-        const user = await User.findOne({
-            where: {
-                email: email
-            }
-        });
+    const user = await User.findOne({
+      where: {
+        email: email,
+      },
+    });
 
-        if (user) {
-            return res.status(httpCodes.UNAUTHORIZED)
-                .json({ msg: "Ya existe un usuario con ese email" });
-        }
+    if (user) {
+      return res
+        .status(httpCodes.UNAUTHORIZED)
+        .json({ msg: 'Ya existe un usuario con ese email' });
+    }
 
-        await User.create({
-            lastName,
-            firstName,
-            email,
-            password: passwordHash,
-        });
+    await User.create({
+      lastName,
+      firstName,
+      email,
+      password: passwordHash,
+    });
 
-        res.status(httpCodes.OK)
-            .json({ msg: "Usuario registrado con éxito", user: firstName, lastName, email, passwordHash });
-    } catch (error) {
-        res.status(httpCodes.BAD_REQUEST)
-            .json({ error, ok: false });
-    };
+    res
+      .status(httpCodes.OK)
+      .json({
+        msg: 'Usuario registrado con éxito',
+        user: firstName,
+        lastName,
+        email,
+        passwordHash,
+      });
+  } catch (error) {
+    res.status(httpCodes.BAD_REQUEST).json({ error, ok: false });
+  }
 };
 
-const encryptPassword = async (password) => {
-    const saltRounds = 10;
-    return await bcrypt.hash(password, saltRounds);
+const encryptPassword = async password => {
+  const saltRounds = 10;
+  return await bcrypt.hash(password, saltRounds);
 };
 
 module.exports = {
-    createUser
+  createUser,
 };
