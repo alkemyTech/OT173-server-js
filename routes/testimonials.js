643ed5eb93da750971f9ee03544669784efb8af5
Nicolás Cabello <cabellonic@gmail.com>
2022-05-04 18:53:50 -0300

    update: Remove unnecessary code

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -5,10 +5,6 @@ const {
   updateTestimonial,
 } = require('../controllers/testimonialsControllers.js');
 
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
-
 router.put('/:id', updateTestimonial);
 
 module.exports = router;
