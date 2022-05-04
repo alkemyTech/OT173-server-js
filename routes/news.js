e96f676ce53c4479c907f67bbc252460bac10ede
Juanhernandezzs <juanmhdz99@gmail.com>
2022-05-04 20:53:42 -0300

    fix: Changes from other branch

--- routes/news.js
+++ routes/news.js
@@ -9,12 +9,10 @@ router.get('/', async function (req, res, next) {
     try {
         const allNews = await Entries.findAll({ where: { type: 'news' } })
         const news = allNews.map(n => {
-            return {
-                id: n.id,
-                name: n.name,
-                image: n.image,
-                createdAt: n.createdAt
-            }
+            n.id,
+            n.name,
+            n.image,
+            n.createdAt
         })
         res.send(news);
     } catch (error) {
