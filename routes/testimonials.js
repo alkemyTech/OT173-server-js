d64a2ea96ac36f8599cf1ff350e0e8a39d7961e0
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-05-06 01:40:44 -0300

    update: add auth to delete controller

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
+const {authRole} = require("../middlewares/authorizationMiddleware.js")
 const {
   updateTestimonial,
+  deleteTestimonial
 } = require('../controllers/testimonialsControllers.js');
 
 router.put('/:id', updateTestimonial);
+
+router.delete('/:id',authRole,deleteTestimonial)
 
 module.exports = router;
