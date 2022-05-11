63d1a26de33f7a923422cd00fe3686c989cb18cc
Mariano Monfort <mmm.129k@gmail.com>
2022-05-11 12:21:23 -0300

    First Commit
    -Se creo el endpoint /member/:id
    
    -Se creo el controlador que recibe los parametros y los actualiza en la base de datos una vez verificados.

--- routes/members.js
+++ routes/members.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { newMembers, getMembers } = require('../controllers/membersControllers');
+const { newMembers, getMembers, editMembers } = require('../controllers/membersControllers');
 const { validateMembers } = require('../validations/validationMembers');
 
 router.post('/', validateMembers, newMembers);
 router.get('/', getMembers);
+router.put('/:id',validateMembers, editMembers);
 
 module.exports = router;
