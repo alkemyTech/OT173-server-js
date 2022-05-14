9fe0b139bb20f870a47ed1df8a9c654949e08481
Nicolás Cabello <cabellonic@gmail.com>
2022-05-14 17:11:03 -0300

    fix: Look for the roleId in the 'user' prop of the decoded token

--- middlewares/authorizationMiddleware.js
+++ middlewares/authorizationMiddleware.js
@@ -1,25 +1,25 @@
-const jwt = require('jsonwebtoken')
-const httpCodes = require('../constants/constants')
+const jwt = require('jsonwebtoken');
+const httpCodes = require('../constants/constants');
 
 const authRole = (req, res, next) => {
-        const token = req.headers['authorization']
-        if (!token) {
-            return res.status(httpCodes.UNAUTHORIZED).send('Access denied')
-        } else {
-            const tokenBody = token.slice(7)
-            jwt.verify(tokenBody, 'secretKey', (err, decoded) => {
-                if (err) {
-                    console.log(`JWT error: ${err}`)
-                    return res.status(httpCodes.UNAUTHORIZED).send('Access denied')
-                }
+  const token = req.headers['authorization'];
+  if (!token) {
+    return res.status(httpCodes.UNAUTHORIZED).send('Access denied');
+  } else {
+    const tokenBody = token.slice(7);
+    jwt.verify(tokenBody, 'secretKey', (err, decoded) => {
+      if (err) {
+        console.log(`JWT error: ${err}`);
+        return res.status(httpCodes.UNAUTHORIZED).send('Access denied');
+      }
 
-                if (decoded.roleId === 1) {
-                    next()
-                } else {
-                    return res.status(httpCodes.FORBIDDEN).send('Access denied')
-                }
-            })
-        }
-    }
+      if (decoded.user.roleId === 1) {
+        next();
+      } else {
+        return res.status(httpCodes.FORBIDDEN).send('Access denied');
+      }
+    });
+  }
+};
 
-module.exports = {authRole}
+module.exports = { authRole };
