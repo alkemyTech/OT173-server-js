f907150239510785420735abaf82da66d1250abe
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-05-13 21:17:01 -0300

    update: validating route

--- routes/members.js
+++ routes/members.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { newMembers, getMembers, updateMembers } = require('../controllers/membersControllers');
+const { newMembers, getMembers, updateMembers,deleteMember } = require('../controllers/membersControllers');
 const { validateMembers } = require('../validations/validationMembers');
 
 router.post('/', validateMembers, newMembers);
 router.get('/', getMembers);
 router.put('/:id',validateMembers, updateMembers);
-
+router.delete("/",validateMembers,deleteMember)
 module.exports = router;
