463d1d9db8c2dc97c284225dd6fa4a25500e6aa4
Nicolás Cabello <cabellonic@gmail.com>
2022-05-14 17:11:58 -0300

    fix: Remove duplicated code

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,14 +1,13 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const {authRole} = require("../middlewares/authorizationMiddleware.js")
+const { authRole } = require('../middlewares/authorizationMiddleware.js');
 const {
   updateTestimonial,
-  deleteTestimonial
+  deleteTestimonial,
 } = require('../controllers/testimonialsControllers.js');
-const httpCodes = require("../constants/constants.js");
-const validationTestimonial = require("../validations/validationsTestimonial.js");
-const {Testimonial} = require("../models/index.js");
-const {updateTestimonial} = require('../controllers/testimonialsControllers.js');
+const httpCodes = require('../constants/constants.js');
+const validationTestimonial = require('../validations/validationsTestimonial.js');
+const { Testimonial } = require('../models/index.js');
 const db = require('../models/index.js');
 
 router.get('/', async function (req, res, next) {
@@ -20,20 +19,24 @@ router.get('/', async function (req, res, next) {
   }
 });
 
-router.post('/',validationTestimonial ,async function (req, res, next) {
-    const {name, image, content} = req.body
-    
-    try {
-        const newTestimonial = await Testimonial.create({name, image, content})        
-        res.status(httpCodes.OK).json({error:false})
-    } catch (error) {
-        res.status(httpCodes.BAD_REQUEST).json({error:true,message:error.message || "Error al enviar el contacto"})
-    }         
- 
+router.post('/', validationTestimonial, async function (req, res, next) {
+  const { name, image, content } = req.body;
+
+  try {
+    const newTestimonial = await Testimonial.create({ name, image, content });
+    res.status(httpCodes.OK).json({ error: false });
+  } catch (error) {
+    res
+      .status(httpCodes.BAD_REQUEST)
+      .json({
+        error: true,
+        message: error.message || 'Error al enviar el contacto',
+      });
+  }
 });
 
 router.put('/:id', updateTestimonial);
 
-router.delete('/:id',authRole,deleteTestimonial)
+router.delete('/:id', authRole, deleteTestimonial);
 
 module.exports = router;
