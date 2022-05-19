d91604667c1a40b162fc19273245e8d9b87e6eb9
Facundo Villarreal <facundovillarreal@mangoodev.com>
2022-05-19 13:48:19 -0300

    se elimaron cambios que no iban en el commit anterior

--- routes/users.js
+++ routes/users.js
@@ -1,93 +1,95 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const db = require("../models");
-const bcrypt = require("bcryptjs");
-const validationLogin = require("../validations/validationsLogin");
-const httpCodes = require("../constants/constants");
-const { createToken, verifyToken, bearerToken } = require("../auth/auth");
-const jwt = require("jsonwebtoken");
-const {authRole} = require('../middlewares/authorizationMiddleware')
-
+const db = require('../models');
+const bcrypt = require('bcryptjs');
+const validationLogin = require('../validations/validationsLogin');
+const httpCodes = require('../constants/constants');
+const { createToken, verifyToken } = require('../auth/auth');
+const jwt = require('jsonwebtoken');
+const { authRole } = require('../middlewares/authorizationMiddleware');
 
 /* GET users listing. */
-router.get('/' , async (req, res, next) => {
-   try{
-      const result = await db.User.findAll();
-      res.send(result)
-    } catch(error) {
-      res.status(httpCodes.BAD_REQUEST).json({ error });
-    } 
+router.get('/', authRole, async (req, res, next) => {
+  try {
+    const result = await db.User.findAll();
+    res.send(result);
+  } catch (error) {
+    res.status(httpCodes.BAD_REQUEST).json({ error });
+  }
 });
 
 /* GET specific user verify */
 router.get('/auth/me', function (req, res, next) {
-  const token = req.headers['authorization']
+  const token = req.headers['authorization'];
   try {
     const verify = verifyToken(token);
-    return  res.json(jwt.decode(token));
-  }catch (error) {
-    res.status(httpCodes.BAD_REQUEST).json({ error, ok: false })
-  }      
+    return res.json(jwt.decode(token));
+  } catch (error) {
+    res.status(httpCodes.BAD_REQUEST).json({ error, ok: false });
+  }
 });
 
-router.delete("/:id", async (req, res) => {
-
+router.delete('/:id', async (req, res) => {
   try {
-    const response = await db.User.destroy({ 
-      where: { 
-        id: req.params.id 
-      } 
-    })
-    
-    if(response === 1){
-      return res.status(httpCodes.OK).json({ msg: "User deleted successfully." })
-    }
-    return res.status(httpCodes.BAD_REQUEST).json({ msg: "An error occurred. Try again." })
+    const response = await db.User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
+    if (response === 1) {
+      return res
+        .status(httpCodes.OK)
+        .json({ msg: 'User deleted successfully.' });
+    }
+
+    return res
+      .status(httpCodes.BAD_REQUEST)
+      .json({ msg: 'An error occurred. Try again.' });
   } catch (error) {
-    res.status(httpCodes.BAD_REQUEST).json({ error, ok: false })
+    res.status(httpCodes.BAD_REQUEST).json({ error, ok: false });
   }
-})
+});
 
 /*Post user login*/
-router.post(
-  "/auth/login",
-  validationLogin,
+router.post('/auth/login', validationLogin, async function (req, res) {
+  try {
+    const user = await db.User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
 
-  async function (req, res) {
-    try {
-      const user = await db.User.findOne({
-        where: {
-          email: req.body.email,
-        },
-      });
-
-      if (!user) {
-        return res.status(httpCodes.UNAUTHORIZED).json({ msg: "Invalid username or password" });
-      }
-
-      const comparePassword = bcrypt.compareSync(
-        req.body.password,
-        user.dataValues.password
-      );
-      if (!comparePassword) {
-        return res.status(httpCodes.UNAUTHORIZED).json({ msg: "Invalid username or password" });
-      }
-
-      const { password, ...userConfirm } = user.dataValues;
-
-      let token = createToken(userConfirm)
-
-      if(token?.ok) {
-        return res.status(httpCodes.OK).json(token);
-      }
-
-      return res.status(httpCodes.UNAUTHORIZED).json(token)
-
-    } catch (error) {
-      res.status(httpCodes.BAD_REQUEST).json({ error, ok: false });
+    if (!user) {
+      return res
+        .status(httpCodes.UNAUTHORIZED)
+        .json({ msg: 'Invalid username or password' });
     }
+
+    const comparePassword = bcrypt.compareSync(
+      req.body.password,
+      user.dataValues.password
+    );
+
+    if (!comparePassword) {
+      return res
+        .status(httpCodes.UNAUTHORIZED)
+        .json({ msg: 'Invalid username or password' });
+    }
+
+    const { password, ...userConfirm } = user.dataValues;
+    const token = createToken(userConfirm);
+
+    if (token?.ok) {
+      return res
+        .status(httpCodes.OK)
+        .json({ token: token.token, user: userConfirm });
+    }
+
+    return res.status(httpCodes.UNAUTHORIZED).json({ msg: token.msg });
+  } catch (error) {
+    res.status(httpCodes.BAD_REQUEST).json({ error, ok: false });
   }
-);
+});
 
 module.exports = router;
